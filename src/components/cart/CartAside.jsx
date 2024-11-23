3e428419d970d0dd22be26860bbf5c25d7b8f1ce
Santiago Peralta <60952230+BehindM3@users.noreply.github.com>
2024-11-23 14:56:14 -0300

    Cart Aside fixed

--- src/components/cart/CartAside.jsx
+++ src/components/cart/CartAside.jsx
@@ -5,25 +5,30 @@ const CartAside = () => {
 
     const { cart } = useContext(cartContext);
 
-    const cartProducts = cart.map( product => {
+    const cartProducts = cart.map(product => {
         return (
             <article className="prod-cart" key={product.id}>
-                <img src={product.img} alt={`Imagen ilustrativa para ${product.nameItem}`} className="prod-img"/>
-                <p className="prod-name">{product.nameItem}</p>
-                <p className="prod-price">{product.quantity}x{product.price}$</p>
-                <button className="dlt-prod-cart">x</button>
+                <div className="info-prod-cart">
+                    <img src={product.img} alt={`Imagen ilustrativa para ${product.nameItem}`} className="prod-img" />
+                    <p className="prod-name">{product.nameItem}</p>
+                    <p className="prod-price">{product.quantity} x {product.price}$</p>
+                </div>
+                <button className="dlt-prod-cart"></button>
             </article>
         );
     })
 
     return (
-        <section>
-            <header>
+        <section className="cart-display">
+            <header className="cart-title">
                 <p>Tu carrito</p>
             </header>
-            <main>
+            <main className="main-cart">
                 {cartProducts}
             </main>
+            <footer className="footer-cart">
+                <button className="buy-btn">Buy</button>
+            </footer>
         </section>
     )
 }
